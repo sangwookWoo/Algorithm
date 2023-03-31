5049f57382a4b5dc20168e5a29c1efdce10d4b28
sangwookWoo <110732135+sangwookWoo@users.noreply.github.com>
2023-03-31 18:27:10 +0900

    [level 0] Title: A로 B 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,14 @@
+def solution(before, after):
+    
+    
+    bf = sorted(list(before))
+    af = sorted(list(after))
+    
+    if bf == af:
+        return 1
+    else :
+        return 0
+    # if :
+    #     return 1
+    # else :
+    #     return 0
