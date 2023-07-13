c5b8dbecf10051e16c76759602066bd1847087fd
sangwookWoo <110732135+sangwookWoo@users.noreply.github.com>
2023-07-14 00:38:47 +0900

    [unrated] Title: 귤 고르기, Time: 63.58 ms, Memory: 24.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+def solution(k, tangerine):
+    total = 0
+    answer = 0
+    for i, v in Counter(tangerine).most_common(k):
+        total += v
+        answer += 1
+        if total >= k:
+            return answer
+        
+        
+
+    return answer
